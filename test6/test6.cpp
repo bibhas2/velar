a7e65f05ab573ff2b6302df6624288439d1058a2
Bibhas B <bibhas2@gmail.com>
2024-12-25 05:34:19 -0500

    Add more test

--- test6/test6.cpp
+++ test6/test6.cpp
@@ -2,13 +2,7 @@
 #include <velar.h>
 #include <cassert>
 
-/*
-Create a function that works as follows.
-- Creates a MappedByteBuffer object with file name "__test.dat", read only false and maximum size 128 bytes.
-- Puts a uint32_t of value 10 in the buffer.
-- Puts a uint64_t of value 20 in the buffer.
-*/  
-void test_read_write() {
+void test_read_write_new() {
     MappedByteBuffer b{ "__test.dat", false, 128 };
 
     uint32_t i32 = 10;
@@ -31,13 +25,29 @@ void test_read_write() {
     assert(sv == "Hello World");
 }
 
-/*
-Create a function called test_readonly that works as follows.
-- Creates a MappedByteBuffer object with file name "__test.dat".
-- Gets a uint32_t from the buffer and asserts it is 10.
-- Puts a uint64_t from the buffer and asserts it is 20.
-- Gets a string_view from the buffer and asserts it is "Hello World".
-*/
+void test_read_write_existing() {
+    MappedByteBuffer b{ "__test.dat", false};
+
+    uint32_t i32 = 10;
+    uint64_t i64 = 20;
+
+    b.put(i32);
+    b.put(i64);
+    b.put("Hello World");
+
+    b.flip();
+
+    b.get(i32);
+    b.get(i64);
+
+    std::string_view sv{};
+    b.get(sv);
+
+    assert(i32 == 10);
+    assert(i64 == 20);
+    assert(sv == "Hello World");
+}
+
 void test_readonly() {
     MappedByteBuffer b{ "__test.dat" };
 
@@ -62,7 +72,8 @@ void test_readonly() {
 
 int main(int argc, char **argv)
 {
-    test_read_write();
+    test_read_write_new();
+    test_read_write_existing();
     test_readonly();
 
     std::remove("__test.dat");
